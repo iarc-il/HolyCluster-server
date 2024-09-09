59f1ea67046adfa6591d3b531a491906920b164a
beiser <roy.beiser@gmail.com>
2024-09-09 10:12:37 +0300

    Added HollySpot class

--- src/db_classes.py
+++ src/db_classes.py
@@ -35,3 +35,25 @@ class DxheatRaw(Base):
                 f"esql={self.esql}, dx_homecall={self.dx_homecall}, comment={self.comment}, "
                 f"flag={self.flag}, band={self.band}, mode={self.mode}, continent_dx={self.continent_dx}, "
                 f"continent_spotter={self.continent_spotter}, dx_locator={self.dx_locator})>")
+
+class HollySpot(Base):
+    __tablename__ = 'holly_spots'
+    date = Column(Date)
+    time = Column(Time)
+    mode = Column(Text)
+    band = Column(Text)
+    frequency = Column(Text)
+    spotter_callsign = Column(Text)
+    spotter_locator = Column(Text)
+    spotter_lat = Column(Text)
+    spotter_lon = Column(Text)
+    dx_callsign = Column(Text)
+    dx_locator = Column(Text)
+    dx_lat = Column(Text)
+    dx_lon = Column(Text)
+
+    def __repr__(self):
+        return(f"<HollySpot(date={self.date}, time={self.time}, mode={self.mode}, band={self.band}, frequency={self.frequency}, "
+               f"spotter_callsign={self.spotter_callsign}, spotter_locator={self.spotter_locator}, "
+               f"spotter_lat={self.spotter_lat},  spotter_lon={self.spotter_lon}, dx_callsign={self.dx_callsign}, "
+               f"dx_locator={self.dx_locator}, dx_lat={self.dx_lat}, dx_lon={self.dx_lon})>")
