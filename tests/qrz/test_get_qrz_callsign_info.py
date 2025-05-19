14a4dfb87d790a4ab28cbe1b4699bc4bc3bd7914
beiser <roy.beiser@gmail.com>
2025-05-19 07:24:45 +0300

    Cleanup

--- tests/qrz/test_get_qrz_callsign_info.py
+++ tests/qrz/test_get_qrz_callsign_info.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+import asyncio
 from loguru import logger
 
 grandparent_folder = Path(__file__).parents[2] # 2 directories up
@@ -12,12 +13,17 @@ from src.settings import (
 from src.qrz import get_qrz_session_key, get_qrz_callsign_info
 
 
+async def main(debug: bool = False):
+  qrz_session_key = get_qrz_session_key(username=QRZ_USER, password=QRZ_PASSOWRD, api_key=QRZ_API_KEY)
+  logger.debug(f"{qrz_session_key}")
+
+
+  callsign="4x5br".upper()
+  # callsign="4x5dd".upper()
+  info = await get_qrz_callsign_info(qrz_session_key=qrz_session_key, callsign=callsign, debug=debug)
+  logger.debug(f"{info=}")
+
+
 debug = True
-qrz_session_key = get_qrz_session_key(username=QRZ_USER, password=QRZ_PASSOWRD, api_key=QRZ_API_KEY)
-logger.debug(f"{qrz_session_key}")
+asyncio.run(main(debug=debug))
 
-
-callsign="4x5br".upper()
-# callsign="4x5dd".upper()
-info = get_qrz_callsign_info(qrz_session_key=qrz_session_key, callsign=callsign, debug=debug)
-logger.debug(f"{info=}")
